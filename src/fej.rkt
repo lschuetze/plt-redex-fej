ffb63b0afcff002465fcb0179375c6b6c808de37
Lars Schütze <lars.schuetze@tu-dresden.de>
2019-12-09 15:12:37 +0100

    Changed syntax writeup

--- src/fej.rkt
+++ src/fej.rkt
@@ -7,71 +7,61 @@
 (define-language fej
 
   ; class declarations
-  (L (class C ((T f) ...) M ... A ... ))
+  (L ::= (class C ((T f) ...) M ... A ... ))
 
   ; role declarations
-  (A (role R requires Mi ... ((T f) ...) M ... ))
+  (A ::= (role R requires Mi ... ((T f) ...) M ... ))
 
   ; type declarations
-  (T
-   (C R)
-   (((C R) ...) C))
+  (T ::= (C R)
+     (((C R) ...) C))
 
   ; method signatures
-  (Mi (T m ((T x) ...)))
+  (Mi ::= (T m ((T x) ...)))
 
   ; types and interfaces
-  (Ts
-   T
-   (Mi ...))
+  (Ts ::= T
+      (Mi ...))
 
   ; method declarations
-  (M (Mi e))
+  (M ::= (Mi e))
 
   ; terms / expressions
-  (e
-   x
-   (lkp e f)
-   (call e m e ...)
-   (new C e ... ⊕ r ...)
-   (bind x ... with r ... from y ... e))
+  (e ::= x
+     (lkp e f)
+     (call e m e ...)
+     (new C e ... ⊕ r ...)
+     (bind x ... with r ... from y ... e))
 
   ; values
-  (v (new C v ... ⊕ r ...))
+  (v ::= (new C v ... ⊕ r ...))
 
   ; role instance
-  (r (v R v ...))
+  (r ::= (v R v ...))
 
   ; class table
-  (CT (L ...))
+  (CT ::= (L ...))
 
   ; evaluation contexts
-  (E
-   hole
-   (lkp E f) ; CR-FIELD
-   (call E m e ...) ; CR-INVK
-   (call v m v ... E e ...) ; CR-INVK-ARG ;; Fig. 16 is not clear here - take definition of FJ
-   (new C v ... E e ...) ; CR-NEW
-   )
+  (E ::= hole
+     (lkp E f) ; CR-FIELD
+     (call E m e ...) ; CR-INVK
+     (call v m v ... E e ...) ; CR-INVK-ARG
+     (new C v ... E e ...) ; CR-NEW
+     )
 
   ; typing environment
-  (Γ ((x T) ...))
+  (Γ ::= ((x T) ...))
 
-  (Bool #t #f)
+  (Bool ::= #t #f)
 
   ; class names
-  (B variable-not-otherwise-mentioned) ; type name
-  (C variable-not-otherwise-mentioned)
-  (D variable-not-otherwise-mentioned)
+  (B C D ::= variable-not-otherwise-mentioned) ; type name
   ; role names
-  (R variable-not-otherwise-mentioned)
+  (R ::= variable-not-otherwise-mentioned)
   ; field names
-  (f variable-not-otherwise-mentioned)
-  (g variable-not-otherwise-mentioned)
+  (f g ::= variable-not-otherwise-mentioned)
   ; method names
-  (m variable-not-otherwise-mentioned)
-  (n variable-not-otherwise-mentioned)
+  (m n ::= variable-not-otherwise-mentioned)
   ; variable names
-  (x variable-not-otherwise-mentioned)
-  (y variable-not-otherwise-mentioned)
-  )
+  (x y := variable-not-otherwise-mentioned))
